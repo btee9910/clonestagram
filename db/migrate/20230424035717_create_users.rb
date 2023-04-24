445dc56c089b0ff7ef69d0c57f9a9f04d9dac8b7
btee9910 <btee9910@gmail.com>
2023-04-24 14:10:22 +1000

    First commit

---
+++ db/migrate/20230424035717_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.text :email
+      t.string :password_digest
+      t.text :display_name
+      t.text :profile_picture
+
+      t.timestamps
+    end
+  end
+end
