19357d720d55833049ba01ff6d1b5ffed9164abf
btee9910 <btee9910@gmail.com>
2023-04-26 15:58:16 +1000

    2nd commit, nothing much, but x10 Speeding up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root :to => 'pages#home'
+
+  resources :users, :only => [ :new, :create]
+
+  # get 'users/new'
+  # get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
